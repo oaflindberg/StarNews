07d2fbb740ccf7a4d3d3500d9085e3663e443032
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2019-10-23 13:01:37 +0200

    Changed articles['published'] from string to int

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $authors = [
     [
         'name' => 'Lando Calrissian',
-        'authorimg' => '',
+        'authorimg' => 'img/lando.png',
         'id' => 1,
         'articles' => [
             [
@@ -13,7 +13,7 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-01',
+                'published' => 19770525,
                 'likes' => 666,
             ],
 
@@ -22,14 +22,14 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-02',
+                'published' => 19770525,
                 'likes' => 666,
             ]
         ]
     ],
     [
         'name' => 'Han Solo',
-        'authorimg' => '',
+        'authorimg' => 'img/han.png',
         'id' => 2,
         'articles' => [
             [
@@ -37,7 +37,7 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-01',
+                'published' => 19770525,
                 'likes' => 666,
             ],
 
@@ -46,14 +46,14 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-02',
+                'published' => 19770525,
                 'likes' => 666,
             ]
         ]
     ],
     [
         'name' => 'Luke Skywalker',
-        'authorimg' => '',
+        'authorimg' => 'img/luke.png',
         'id' => 3,
         'articles' => [
             [
@@ -61,7 +61,7 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-01',
+                'published' => 19770525,
                 'likes' => 666,
             ],
 
@@ -70,14 +70,14 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-02',
+                'published' => 19770525,
                 'likes' => 666,
             ]
         ]
     ],
     [
         'name' => 'Obi-Wan',
-        'authorimg' => '',
+        'authorimg' => 'img/obi-wan.png',
         'id' => 4,
         'articles' => [
             [
@@ -85,7 +85,7 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-01',
+                'published' => 19770525,
                 'likes' => 666,
             ],
 
@@ -94,14 +94,14 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-02',
+                'published' => 19770525,
                 'likes' => 666,
             ]
         ]
     ],
     [
         'name' => 'Darth Vader',
-        'authorimg' => '',
+        'authorimg' => 'img/darth.png',
         'id' => 5,
         'articles' => [
             [
@@ -109,7 +109,7 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-01',
+                'published' => 19770525,
                 'likes' => 666,
             ],
 
@@ -118,7 +118,7 @@ $authors = [
                 'content' => "Roonan aayla -lom boba shimrra darth. Anthos veila meridian utapau huk. Wat ree-yees biggs qui-gon amidala poggle. Gora breha ric organa karrde rune nar wharl rhen. Cal lorth ben wyl muzzer miraluka. Omas ackbar nadd thistleborn derek anomid ongree. Mod wicket kowakian hutt zuckuss endor ralter auril mara. H'nemthean mothma biggs stele priapulin mas su. Ponda oppo wroonian snivvian mod. Chewbacca jabba keshiri d8 psadan qui-gonn quadrinaros sern. Luminara cadavine grizmallt trandoshan sebulba. Klivian klivian bith anx.
                  Nassau rebo wol beru wampa naboo derlin. Dantooine evazan desann vong ka yaddle firmus quence. Naboo drovian raioballo windu. Gerb mustafar lobot epicanthix anzati gamorr rotta. Watto shi'ido roos offee ikrit jabba shmi kalee. Ric noa dodonna colton. Kessel iktotchi bibble solo evazan warrick desann dunwell depa. Epicanthix kenobi kasan logray troig wesell ahsoka. Max grodin chazrach calamari bothan x1 calrissian. Kathol hypori luke sidious himoran subterrel wampa khai shaak. Pau'an moff chistori elomin kwa ors iblis.
                  Gotal kenobi ben tion lobot gora emtrey frozian. Droopy saffa massassi taun rebo vella iktotchi. Calamari chistori obi-wan mygeeto jade yavin darth. Solo chistori thrawn hobbie corran gossam felucia logray. Owen han gizka rebo dug cade. Keyan rhen gallia quermian mothma biggs gorog choka dantooine. Sikan sullustan vilim r2-d2 jamillia tsavong grievous endocott. Ebe cognus qel-droma ongree sluis ka durge. Sidious bria ken gand kota defel. Hutt coway yoda woostoid c-3po tatooine darth yoda ti. Wharl binks letaki raa.",
-                'published' => '2019-01-02',
+                'published' => 19770525,
                 'likes' => 666,
             ]
         ]
