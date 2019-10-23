97aabf5cbf080b868a9931ab26d41e8ef1e49cba
oaflindberg <45793849+oaflindberg@users.noreply.github.com>
2019-10-23 10:38:04 +0200

    Added authors to data.php, favicon and styling.

--- index.php
+++ index.php
@@ -10,10 +10,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Plain News</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon/coffee-cup.png" type="image/x-icon">
 </head>
 
 <body>
+    <nav>
+        <h1>Plain News!</h1>
+    </nav>
+    <main>
+        <article>
+
+        </article>
+    </main>
 
 </body>
 
